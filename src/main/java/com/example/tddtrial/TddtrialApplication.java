56fc7e49ec0f6d004e7c991b632bfde1b9997cb2
Alberto2708 <mentadoreyescarlosalberto@gmail.com>
2025-03-31 18:47:04 -0600

    first commit

---
+++ src/main/java/com/example/tddtrial/TddtrialApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddtrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddtrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddtrialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tddtrial/TddtrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tddtrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddtrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
